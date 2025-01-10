b54b766198f962788d81cf13eedcb2cf58476056
misLuigi <162457852+misLuigi@users.noreply.github.com>
2025-01-11 08:38:17 +0900

    Added the score functionality to the guest area and now the buttons are functioning properly.

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
 const scoreHome = document.getElementById("home-score-display");
 console.log(scoreHome);
 
-const plusOneBtn = document.getElementById("home-add-pts-btn1");
-console.log(plusOneBtn);
+const homePlusOneBtn = document.getElementById("home-add-pts-btn1");
+console.log(homePlusOneBtn);
 
+const homePlusTwoBtn = document.getElementById("home-add-pts-btn2");
+console.log(homePlusTwoBtn);
+
+const homePlusThreeBtn = document.getElementById("home-add-pts-btn3");
+console.log(homePlusThreeBtn);
+
+//home score function area
 let homeScore = 0
-plusOneBtn.addEventListener("click", function addOne() {
-  scoreHome.textContent = homeScore += 1
+
+homePlusOneBtn.addEventListener("click", function homeAddOne() {
+  scoreHome.textContent = homeScore += 1;
 })
+
+homePlusTwoBtn.addEventListener("click", function homeAddTwo() {
+  scoreHome.textContent = homeScore += 2;
+})
+
+homePlusThreeBtn.addEventListener("click", function homeAddThree() {
+  scoreHome.textContent = homeScore += 3;
+})
+
+//guest area
+
+const scoreGuest = document.getElementById("guest-score-display");
+console.log(scoreGuest);
+
+const guestPlusOneBtn = document.getElementById("guest-add-pts-btn1");
+console.log(guestPlusOneBtn);
+
+const guestPlusTwoBtn = document.getElementById("guest-add-pts-btn2");
+console.log(guestPlusTwoBtn);
+
+const guestPlusThreeBtn = document.getElementById("guest-add-pts-btn3");
+console.log(guestPlusThreeBtn);
+
+//guest score funciton area
+
+let guestScore = 0
+
+guestPlusOneBtn.addEventListener("click", function guestAddOne() {
+  scoreGuest.textContent = guestScore += 1;
+})
+
+guestPlusTwoBtn.addEventListener("click", function guestAddTwo() {
+  scoreGuest.textContent = guestScore += 2;
+})
+
+guestPlusThreeBtn.addEventListener("click", function guestAddThree() {
+  scoreGuest.textContent = guestScore += 3;
+})
+
